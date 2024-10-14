60e0045413c21519aafc745de18c3252dd82aa32
JensBrandels <brandelsjens@gmail.com>
2024-10-14 12:41:58 +0200

    testtest

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage.tsx";
 
 function App() {
   return (
